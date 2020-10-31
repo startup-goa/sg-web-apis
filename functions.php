6de98c515f50d8880f34e94a793054094d5d6f0f
Siddharth Ashok <siddharthashokg@gmail.com>
2020-10-31 15:29:16 +0530

    ACF settings

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@
 include("inc/custom-posts.php");
 include("inc/custom-taxonomies.php");
 
+include("inc/acf-meta.php");
+include("inc/acf-settings.php");
+
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );

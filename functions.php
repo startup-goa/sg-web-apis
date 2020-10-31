1479e1091d65998715b45531aab5907856aeff9b
Siddharth Ashok <siddharthashokg@gmail.com>
2020-10-31 14:49:13 +0530

    initial blank wp theme

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Startup Goa functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Startup_Goa
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+
+
+
+
+
